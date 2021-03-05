4a47b866199c5cf9f06cdf1e2bdf84a932d61b9a
ted523 <tedotieno523@gmail.com>
2021-03-05 17:10:46 +0300

    Django Unittest

--- boards_app/boards/urls.py
+++ boards_app/boards/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
 
 	path('', views.home, name='home'),
+	path('boards/<str:pk>/', views.board_topics, name='board_topics')
 
 ]
---
+++ boards_app/boards/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Board
+from django.http import Http404
+
+# Create your views here.
+
+def home(request):
+	boards = Board.objects.all()
+	return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+	try:
+		board = Board.objects.get(pk=pk)
+	except Board.DoesNotExist:
+		raise Http404
+	return render(request, 'topics.html', {'board': board})
