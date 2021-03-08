d73a429dbacdb944dbb4fbde135b087970831b08
ted523 <tedotieno523@gmail.com>
2021-03-08 17:33:05 +0300

    Base template completed

--- boards_app/boards/urls.py
+++ boards_app/boards/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns=[
 
 	path('', views.home, name='home'),
-	path('boards/<str:pk>/', views.board_topics, name='board_topics')
+	path('boards/<str:pk>/', views.board_topics, name='board_topics'),
+	path('boards/<str:pk>/new/', views.new_topic, name='new_topic')
 
 ]
