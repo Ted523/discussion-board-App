a413e0fb575959ec955a59481a25a3c66e99c3f2
ted523 <tedotieno523@gmail.com>
2021-03-11 17:14:50 +0300

    Form API Implementation

--- boards_app/boards/views.py
+++ boards_app/boards/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Board, Topic, Post
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
+
 
 
 # Create your views here.
@@ -17,25 +19,24 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
 	board = get_object_or_404(Board, pk=pk)
+	user = User.objects.first() #To get the current logged in user
 
 	if request.method == 'POST':
-		subject = request.POST['subject']
-		message = request.POST['message']
+		
+		form = NewTopicForm(request.POST)
+		if form.is_valid():
+			topic = form.save(commit=False)
+			topic.board = board
+			topic.starter = user
+			topic.save()
 
-		user = User.objects.first() # To get the currently logged user
-
-		topic = Topic.objects.create(
-			subject = subject,
-			board = board,
-			starter = user
-		)
-
-		post = Post.objects.create(
-			message = message,
-			topic = topic,
-			created_by = user
-		)
-
-		return redirect('board_topics', pk=board.pk) #redirect to the created topics page
-
-	return render(request, 'new_topic.html', {'board': board})
+			post = Post.objects.create(
+				message=form.cleaned_data.get('message'),
+				topic=topic,
+				created_by=user
+			)
+			return redirect('board_topics', pk=board.pk) #redirect to the created topics page
+		
+	else:
+		form = NewTopicForm()
+	return render(request, 'new_topic.html', {'board': board, 'form': form})
